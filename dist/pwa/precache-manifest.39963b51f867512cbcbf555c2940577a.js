971dfae04b5cc3a9edc0eb5f81d33a5c35e9f536
Luboš Matejčík <lma@panter.ch>
2024-03-12 18:09:07 +0100

    chore: add dist folder

---
+++ dist/pwa/precache-manifest.39963b51f867512cbcbf555c2940577a.js
@@ -0,0 +1,678 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c44be6fd6c4892906ec",
+    "url": "/css/2.b547c512.css"
+  },
+  {
+    "revision": "e77deb64bf8925b2ff86",
+    "url": "/css/app.766a7835.css"
+  },
+  {
+    "revision": "9588e2362b4a501c0a0a204ffc043e3d",
+    "url": "/fonts/KFOkCnqEu92Fr1MmgVxGIzQ.9588e236.woff"
+  },
+  {
+    "revision": "3d9446f210892af971a3854d0f984ae9",
+    "url": "/fonts/KFOlCnqEu92Fr1MmEU9fChc-.3d9446f2.woff"
+  },
+  {
+    "revision": "f4b08b5a857ef12352aaf0ebc1316460",
+    "url": "/fonts/KFOlCnqEu92Fr1MmSU5fChc-.f4b08b5a.woff"
+  },
+  {
+    "revision": "d0cc855e64aca1072711f49d1a38ca0c",
+    "url": "/fonts/KFOlCnqEu92Fr1MmWUlfChc-.d0cc855e.woff"
+  },
+  {
+    "revision": "51db4691b982b906d8cd9de2e2c230fd",
+    "url": "/fonts/KFOlCnqEu92Fr1MmYUtfChc-.51db4691.woff"
+  },
+  {
+    "revision": "d679a90608a65cc9394f67448ca2094a",
+    "url": "/fonts/KFOmCnqEu92Fr1Mu7GxM.d679a906.woff"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "0916f7aad1b6187a4740c0d8574a2672",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c44be6fd6c4892906ec",
+    "url": "/js/2.3aa5cf6f.js"
+  },
+  {
+    "revision": "1034f809bf9b2dad6695",
+    "url": "/js/3.fd776d9a.js"
+  },
+  {
+    "revision": "0ef3273e5ffc903bb39b",
+    "url": "/js/4.dcfe2b1c.js"
+  },
+  {
+    "revision": "e77deb64bf8925b2ff86",
+    "url": "/js/app.e0529737.js"
+  },
+  {
+    "revision": "a1e130c5a11804760177",
+    "url": "/js/vendor.a7b3a3b5.js"
+  },
+  {
+    "revision": "e89c7e6cb4c02169b7d0bf2dcdea99c7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "043ea4d7587cece6cfa81560ae711def",
+    "url": "/statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "1313b60d059ce5f6660d4d4f8b1d3947",
+    "url": "/statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "7d5480204c050a3a456b25aec22dc425",
+    "url": "/statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "7823099ebeb2f8ae6df009e33cac66ce",
+    "url": "/statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "faae4f8a8b027ba39204c040b2ec2f18",
+    "url": "/statics/icons/favicon.ico"
+  },
+  {
+    "revision": "043ea4d7587cece6cfa81560ae711def",
+    "url": "/statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "5599be3a1552f617b374181d5336e0a8",
+    "url": "/statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "e74e409b5ce64f377b903f3e9046578e",
+    "url": "/statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "eb04aa7ff1fa4be382022f346b783328",
+    "url": "/statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "5bd3ec20d459e476ebd1b68275953dd3",
+    "url": "/statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "7f1a14f81dfc9d8c50116adf0739b05a",
+    "url": "/statics/speech/baby a cecky.mp3"
+  },
+  {
+    "revision": "1ad7293f3bedf777c4ed70e45eee2e74",
+    "url": "/statics/speech/baby a cecky.ogg"
+  },
+  {
+    "revision": "9ffa0f0b26a99932c3099e9e4524cd26",
+    "url": "/statics/speech/bankokot.mp3"
+  },
+  {
+    "revision": "ec7796d872cc5ac757af83ebecff90e5",
+    "url": "/statics/speech/bankokot.ogg"
+  },
+  {
+    "revision": "f5542b3e399d4f2e8bde18a7bbed1bbd",
+    "url": "/statics/speech/byl jsem pochcán.mp3"
+  },
+  {
+    "revision": "424366fd2fad681034393cb116587285",
+    "url": "/statics/speech/byl jsem pochcán.ogg"
+  },
+  {
+    "revision": "d6cbdb4a7294697dda7e304c1bc10f19",
+    "url": "/statics/speech/běž si ho vyhonit.mp3"
+  },
+  {
+    "revision": "c8df116cc1da3b92b5658ade8d9716fa",
+    "url": "/statics/speech/běž si ho vyhonit.ogg"
+  },
+  {
+    "revision": "357bdb5ee368b3a6ffda977ef7e75084",
+    "url": "/statics/speech/co se to tu děje.mp3"
+  },
+  {
+    "revision": "43acde9c2013db40b31900216131e280",
+    "url": "/statics/speech/co se to tu děje.ogg"
+  },
+  {
+    "revision": "28dde8dc356809a1b60f99e625b1af7c",
+    "url": "/statics/speech/co tak čumíš.mp3"
+  },
+  {
+    "revision": "c6b1928ca6b71a0bc31d080e8138c117",
+    "url": "/statics/speech/co tak čumíš.ogg"
+  },
+  {
+    "revision": "cc254d60ac33cdd02f32ec4d85fa462d",
+    "url": "/statics/speech/dort mám i na koulích.mp3"
+  },
+  {
+    "revision": "3af89039eba421acae0a6f38c11405ec",
+    "url": "/statics/speech/dort mám i na koulích.ogg"
+  },
+  {
+    "revision": "4ce431ee8d12cdf1fec3da0a3dc21dba",
+    "url": "/statics/speech/drazí jebáci.mp3"
+  },
+  {
+    "revision": "f86b1a273047fb0f088b933abaff2784",
+    "url": "/statics/speech/drazí jebáci.ogg"
+  },
+  {
+    "revision": "66a6e5c3f44cf310d8179774f779e7d0",
+    "url": "/statics/speech/dík žes mě pozval.mp3"
+  },
+  {
+    "revision": "28f7e40bd64a7cf17192b05f8ba4513c",
+    "url": "/statics/speech/dík žes mě pozval.ogg"
+  },
+  {
+    "revision": "6078a1319bffe9598331164ba2f17aeb",
+    "url": "/statics/speech/děláš chlapům jenom ostudu.mp3"
+  },
+  {
+    "revision": "b8369ad203e0e24cda4d81be94b26e2a",
+    "url": "/statics/speech/děláš chlapům jenom ostudu.ogg"
+  },
+  {
+    "revision": "af9814c884ba515c752b8028132e6342",
+    "url": "/statics/speech/gratuluj semenu.mp3"
+  },
+  {
+    "revision": "053cc63c4a82cd696e182dbc5481820c",
+    "url": "/statics/speech/gratuluj semenu.ogg"
+  },
+  {
+    "revision": "c047a65266ca9a82f4ad02be6d7c16c8",
+    "url": "/statics/speech/haha.mp3"
+  },
+  {
+    "revision": "742f2fe04a977b12dfd08e276e86dcb1",
+    "url": "/statics/speech/haha.ogg"
+  },
+  {
+    "revision": "cbce29f3c6fca3a8b877651ea56a22e8",
+    "url": "/statics/speech/hahaha.mp3"
+  },
+  {
+    "revision": "966ef8904f36fa1efca4d593e0be850d",
+    "url": "/statics/speech/hahaha.ogg"
+  },
+  {
+    "revision": "3dd04f17ffb1e1f0c9ea9c0644650765",
+    "url": "/statics/speech/hou hou hou příchází stifler klaus.mp3"
+  },
+  {
+    "revision": "67793c2284986e796cfb4a4ad1eba245",
+    "url": "/statics/speech/hou hou hou příchází stifler klaus.ogg"
+  },
+  {
+    "revision": "87daef41cc10d779dfcc1c8e9bd6dc64",
+    "url": "/statics/speech/huhaha.mp3"
+  },
+  {
+    "revision": "15fe1a3982e4296ab2d9bfa2debbf723",
+    "url": "/statics/speech/huhaha.ogg"
+  },
+  {
+    "revision": "c6b23e82fa8e638ac9d56b8c3bdb1e0a",
+    "url": "/statics/speech/huhahaha.mp3"
+  },
+  {
+    "revision": "f3e62d88a6f2931c87459125590a8a8e",
+    "url": "/statics/speech/huhahaha.ogg"
+  },
+  {
+    "revision": "1074b805591e1f0738c767829e20292b",
+    "url": "/statics/speech/huhohoha.mp3"
+  },
+  {
+    "revision": "dc3d01314ec300a77bb56a4b090eedc7",
+    "url": "/statics/speech/huhohoha.ogg"
+  },
+  {
+    "revision": "3d5d2bf0663ffb29b1080e48b37fe96a",
+    "url": "/statics/speech/jak se vede kočko.mp3"
+  },
+  {
+    "revision": "b4a40f1dfaf0c6f33b714ca720c9aaff",
+    "url": "/statics/speech/jak se vede kočko.ogg"
+  },
+  {
+    "revision": "5747733106e6a36e364cc952566da525",
+    "url": "/statics/speech/jděte si ho vykouřit.mp3"
+  },
+  {
+    "revision": "99aff385ec258414fdc71e64fd8f1008",
+    "url": "/statics/speech/jděte si ho vykouřit.ogg"
+  },
+  {
+    "revision": "0e31dffb466a0553d869b63f9b61336f",
+    "url": "/statics/speech/jedem do chicaga omrknout kozy.mp3"
+  },
+  {
+    "revision": "86dc52b9d5b988735ad87584684da3c5",
+    "url": "/statics/speech/jedem do chicaga omrknout kozy.ogg"
+  },
+  {
+    "revision": "5facca2d54364bee80757a108d9e22b3",
+    "url": "/statics/speech/jsem stylovej.mp3"
+  },
+  {
+    "revision": "adcc2de5c87b9269a82b283fc0e7a512",
+    "url": "/statics/speech/jsem stylovej.ogg"
+  },
+  {
+    "revision": "2f080f3d4333f8fdc0c7ed4f36365e8f",
+    "url": "/statics/speech/já se poseru.mp3"
+  },
+  {
+    "revision": "c4c74d8de3ef3adb7f83d7ba076c294c",
+    "url": "/statics/speech/já se poseru.ogg"
+  },
+  {
+    "revision": "db7c39c41aedd01dbe98e59aa4f88eb5",
+    "url": "/statics/speech/já se snad pobleju.mp3"
+  },
+  {
+    "revision": "d0a7afc1fc6c8d502df23ece804bd7bf",
+    "url": "/statics/speech/já se snad pobleju.ogg"
+  },
+  {
+    "revision": "9229bd565be2d2a952b63ea75b576d9a",
+    "url": "/statics/speech/já tam budu řádit.mp3"
+  },
+  {
+    "revision": "2165f2a587aa8a22b92c1e33c12dc14e",
+    "url": "/statics/speech/já tam budu řádit.ogg"
+  },
+  {
+    "revision": "834d66069c34646d593c8eee4819cb48",
+    "url": "/statics/speech/kamaráde můžeš mi vylízat prdel.mp3"
+  },
+  {
+    "revision": "634de51b5dd3952a19cc4dafad966cb2",
+    "url": "/statics/speech/kamaráde můžeš mi vylízat prdel.ogg"
+  },
+  {
+    "revision": "0aa4d755a29a6ca577aa1febd63ac8fd",
+    "url": "/statics/speech/každej by to chtěl se stifmeistrem.mp3"
+  },
+  {
+    "revision": "45954030fdf95363eab403c06648c388",
+    "url": "/statics/speech/každej by to chtěl se stifmeistrem.ogg"
+  },
+  {
+    "revision": "b62888d3feb9e3d56963907e9564ded1",
+    "url": "/statics/speech/kdo je tu čuramet.mp3"
+  },
+  {
+    "revision": "562f460e53c680e83881ecc0ef12ad9e",
+    "url": "/statics/speech/kdo je tu čuramet.ogg"
+  },
+  {
+    "revision": "ca10d51f5dcf3eddd66ae5718085edd7",
+    "url": "/statics/speech/konec hry.mp3"
+  },
+  {
+    "revision": "ba80ffd01cd6f063e01aebfc39ac2774",
+    "url": "/statics/speech/konec hry.ogg"
+  },
+  {
+    "revision": "c46c259962cd5d4f078b9766b8711d51",
+    "url": "/statics/speech/kurevská paráda.mp3"
+  },
+  {
+    "revision": "158e298995f23b285f333289dc95a94b",
+    "url": "/statics/speech/kurevská paráda.ogg"
+  },
+  {
+    "revision": "a0a4c81a14ff37adcf1db0480852184f",
+    "url": "/statics/speech/kurva dobrej dort.mp3"
+  },
+  {
+    "revision": "cb451edd81802a2d96cfd3a7c9815603",
+    "url": "/statics/speech/kurva dobrej dort.ogg"
+  },
+  {
+    "revision": "60a079aa3f1b2f5d29fa52301162f14b",
+    "url": "/statics/speech/kurva notak.mp3"
+  },
+  {
+    "revision": "ff56702e24ecfe5bc1be3cb9b43db470",
+    "url": "/statics/speech/kurva notak.ogg"
+  },
+  {
+    "revision": "812d80147d1255281acd786505397933",
+    "url": "/statics/speech/lisianhnus.mp3"
+  },
+  {
+    "revision": "31d79fd3764dc8e6ea35a982c9676f05",
+    "url": "/statics/speech/lisianhnus.ogg"
+  },
+  {
+    "revision": "49d94d205753b0c1bf754dc8f60ec59e",
+    "url": "/statics/speech/mám polevu i mezi půlkama.mp3"
+  },
+  {
+    "revision": "1e89aca933652cbfc45329a6b4c7852b",
+    "url": "/statics/speech/mám polevu i mezi půlkama.ogg"
+  },
+  {
+    "revision": "8cd3ad13579ca5dcff1e031144f22e07",
+    "url": "/statics/speech/mám rád starý lidi.mp3"
+  },
+  {
+    "revision": "b268b066799679769810cb8a52f8f9a5",
+    "url": "/statics/speech/mám rád starý lidi.ogg"
+  },
+  {
+    "revision": "135dedeefa853bdf6de661c96d17661a",
+    "url": "/statics/speech/nejsou kundy, není tanec.mp3"
+  },
+  {
+    "revision": "8ad657e208103bd83572b32631799ea5",
+    "url": "/statics/speech/nejsou kundy, není tanec.ogg"
+  },
+  {
+    "revision": "096c76e87b92c63e433786bc63fc2d38",
+    "url": "/statics/speech/nevzdávejte se a jděte si za svým.mp3"
+  },
+  {
+    "revision": "864a9572212608e3735e59aa7cb0744d",
+    "url": "/statics/speech/nevzdávejte se a jděte si za svým.ogg"
+  },
+  {
+    "revision": "890d53259d78a11cc46a89614a5bac64",
+    "url": "/statics/speech/návod k čurákovi.mp3"
+  },
+  {
+    "revision": "fca0d75c47dc318f9a0cedfcf0698f40",
+    "url": "/statics/speech/návod k čurákovi.ogg"
+  },
+  {
+    "revision": "adec67b7e8d2d5a975bcb6f104eb80a2",
+    "url": "/statics/speech/o čem se to bavíme.mp3"
+  },
+  {
+    "revision": "f70f4f43dc5b65dca5424c3e4c059e2c",
+    "url": "/statics/speech/o čem se to bavíme.ogg"
+  },
+  {
+    "revision": "616599260e566e74dc94423f7ff447ba",
+    "url": "/statics/speech/po tréninku mě můžeš přeblafnout.mp3"
+  },
+  {
+    "revision": "2b713c4a2a71532ba0b5a36ae97cdb26",
+    "url": "/statics/speech/po tréninku mě můžeš přeblafnout.ogg"
+  },
+  {
+    "revision": "967e9bcd943c7eb97f10c30dc0c40c74",
+    "url": "/statics/speech/poslouchej ty řiťopichu.mp3"
+  },
+  {
+    "revision": "5076f49bbb818a12fbab646efdea3a46",
+    "url": "/statics/speech/poslouchej ty řiťopichu.ogg"
+  },
+  {
+    "revision": "fc6b803243cdb366cbcda33bf2e042e6",
+    "url": "/statics/speech/používej kurva mozek.mp3"
+  },
+  {
+    "revision": "00e4710701f3995b3afba28c77c58ae4",
+    "url": "/statics/speech/používej kurva mozek.ogg"
+  },
+  {
+    "revision": "6b29dfc44abbe9d3e3bec9d7b33e59d7",
+    "url": "/statics/speech/píčusi.mp3"
+  },
+  {
+    "revision": "01d35ece6593fb9c65e0b4ad6d4044e8",
+    "url": "/statics/speech/píčusi.ogg"
+  },
+  {
+    "revision": "c84a873d3caa9e5aae9a0be886bcfef9",
+    "url": "/statics/speech/rezervace.mp3"
+  },
+  {
+    "revision": "ce65f5aa4a4c49c6befce85bf38efe77",
+    "url": "/statics/speech/rezervace.ogg"
+  },
+  {
+    "revision": "42e00e3e11e2512c29d32ad71f28a0aa",
+    "url": "/statics/speech/samantiny cukrovinky.mp3"
+  },
+  {
+    "revision": "f114b46aeca4357cae2a41fd7c2b4a41",
+    "url": "/statics/speech/samantiny cukrovinky.ogg"
+  },
+  {
+    "revision": "3032a971566658e44097a8287d55b937",
+    "url": "/statics/speech/seru na to.mp3"
+  },
+  {
+    "revision": "29ea026ee745f81c436444a4fe2dc51f",
+    "url": "/statics/speech/seru na to.ogg"
+  },
+  {
+    "revision": "0e3e0300a3ce24f59f77c1fd2996eb83",
+    "url": "/statics/speech/tak jak zasraný parťáci.mp3"
+  },
+  {
+    "revision": "15c52e6e5cec0710f1fd7b6033a1404e",
+    "url": "/statics/speech/tak jak zasraný parťáci.ogg"
+  },
+  {
+    "revision": "e09dcf9b74f253d5c4c8c0a09fa61f82",
+    "url": "/statics/speech/tak to jsou teplí.mp3"
+  },
+  {
+    "revision": "ebc8de6c7e69e52c16a286a54ad9e32b",
+    "url": "/statics/speech/tak to jsou teplí.ogg"
+  },
+  {
+    "revision": "28eab678043c8327c551005f7298d915",
+    "url": "/statics/speech/ten chlap mě fakt chtěl vojet.mp3"
+  },
+  {
+    "revision": "65d18d2d8ea936e7eaecb8234953d1b6",
+    "url": "/statics/speech/ten chlap mě fakt chtěl vojet.ogg"
+  },
+  {
+    "revision": "18cffab3fb35ddd5790952aa44b6c375",
+    "url": "/statics/speech/ten čokl je skvělej.mp3"
+  },
+  {
+    "revision": "0d591b29a5228aefba26316a1370c1a7",
+    "url": "/statics/speech/ten čokl je skvělej.ogg"
+  },
+  {
+    "revision": "0b9b29eabc65610fbec36ab44db2712e",
+    "url": "/statics/speech/teď do toho jdeme na vostro šmejde.mp3"
+  },
+  {
+    "revision": "9132695c1c70201b82ee070945237522",
+    "url": "/statics/speech/teď do toho jdeme na vostro šmejde.ogg"
+  },
+  {
+    "revision": "e384f82ea9288b106fac0f78bbe8135e",
+    "url": "/statics/speech/to byl ale kretén.mp3"
+  },
+  {
+    "revision": "fc2faffc4468fdf265c1d2c034f49012",
+    "url": "/statics/speech/to byl ale kretén.ogg"
+  },
+  {
+    "revision": "9606952026c30958ad7317f2b64ff6d9",
+    "url": "/statics/speech/to je fantazie.mp3"
+  },
+  {
+    "revision": "3315868711544433bfdd1062b8da90d4",
+    "url": "/statics/speech/to je fantazie.ogg"
+  },
+  {
+    "revision": "a7ef019614cfcc32537414d86aa63b3b",
+    "url": "/statics/speech/to je kurva nechutný.mp3"
+  },
+  {
+    "revision": "db656bd52ed6723bf3937e2dc68ee58c",
+    "url": "/statics/speech/to je kurva nechutný.ogg"
+  },
+  {
+    "revision": "8447ff8ab63ee96f0b394e55b03fe7c2",
+    "url": "/statics/speech/to vás vy kokoti na vejšce nenanučili.mp3"
+  },
+  {
+    "revision": "23d46c67c075de436cf2ac9ef1b7a732",
+    "url": "/statics/speech/to vás vy kokoti na vejšce nenanučili.ogg"
+  },
+  {
+    "revision": "a3baf2ddb41f70f189e675e0ae0f7421",
+    "url": "/statics/speech/ty by sis ani neoholil koule.mp3"
+  },
+  {
+    "revision": "c19ebe5f0d86d20135c2f19397577044",
+    "url": "/statics/speech/ty by sis ani neoholil koule.ogg"
+  },
+  {
+    "revision": "3c0c19673353253ca6066db1a6ee1bbe",
+    "url": "/statics/speech/ty ses snad úplně posral.mp3"
+  },
+  {
+    "revision": "1074b5dcb49c4d8c116d337b42f18a01",
+    "url": "/statics/speech/ty ses snad úplně posral.ogg"
+  },
+  {
+    "revision": "0802717355d855b181a4819318aa8b94",
+    "url": "/statics/speech/ty seš ale medvěd.mp3"
+  },
+  {
+    "revision": "0e330a76d96bcb1a5f040f6422539a1a",
+    "url": "/statics/speech/ty seš ale medvěd.ogg"
+  },
+  {
+    "revision": "8e03dd6d6de97bb684073ff4d39cd643",
+    "url": "/statics/speech/ty vyjebanče.mp3"
+  },
+  {
+    "revision": "503dee173c0d0de6628fedb39729d6c3",
+    "url": "/statics/speech/ty vyjebanče.ogg"
+  },
+  {
+    "revision": "5e042fc3430516b1be2427299d8e08ce",
+    "url": "/statics/speech/uhuhohaha.mp3"
+  },
+  {
+    "revision": "456f1d43d61e107e3c3e3d820bc3afa8",
+    "url": "/statics/speech/uhuhohaha.ogg"
+  },
+  {
+    "revision": "3144464fbdc1bab970b046dd97cf90ce",
+    "url": "/statics/speech/uhulahh.mp3"
+  },
+  {
+    "revision": "01e6f54ca6f37421a0c1941a789d739e",
+    "url": "/statics/speech/uhulahh.ogg"
+  },
+  {
+    "revision": "ffec61edec6826cf85277a91f6e5db77",
+    "url": "/statics/speech/uslyším haleluja.mp3"
+  },
+  {
+    "revision": "baddb10417acf635d888c3772b3f7a82",
+    "url": "/statics/speech/uslyším haleluja.ogg"
+  },
+  {
+    "revision": "80816b0874e0fc672a4d1c5c2c34d2c0",
+    "url": "/statics/speech/uspořádáme rozlučku se svobodou.mp3"
+  },
+  {
+    "revision": "0919a8b202e2d0331ce8cf50d7fda074",
+    "url": "/statics/speech/uspořádáme rozlučku se svobodou.ogg"
+  },
+  {
+    "revision": "507ae6232a68f62acd4cd8693a5379f5",
+    "url": "/statics/speech/už se toho průseru nemůžu dočkat.mp3"
+  },
+  {
+    "revision": "977f89e402b72e4ebfeb62ad9fd06882",
+    "url": "/statics/speech/už se toho průseru nemůžu dočkat.ogg"
+  },
+  {
+    "revision": "062a6f5719d510c183c4fa18a684a4df",
+    "url": "/statics/speech/voni jsou teplí.mp3"
+  },
+  {
+    "revision": "2b267701f92877581cf581c9b28168fe",
+    "url": "/statics/speech/voni jsou teplí.ogg"
+  },
+  {
+    "revision": "32ba40d8622c80ba35d3aacb868a8677",
+    "url": "/statics/speech/vyhul mi hůl.mp3"
+  },
+  {
+    "revision": "0169bd1a0b7e0724eff11b8d5ffc9907",
+    "url": "/statics/speech/vyhul mi hůl.ogg"
+  },
+  {
+    "revision": "c33a4aa0d970ae5d46737bccb3c16f7b",
+    "url": "/statics/speech/vyjebanej mozku.mp3"
+  },
+  {
+    "revision": "f0fd554b549ab5fd70f81662aee07d14",
+    "url": "/statics/speech/vyjebanej mozku.ogg"
+  },
+  {
+    "revision": "721c5cfba1ecf939166cb23c1a47e739",
+    "url": "/statics/speech/vyser se na blbou švadlenku.mp3"
+  },
+  {
+    "revision": "639998fc447797d04d4d8158d07ad77d",
+    "url": "/statics/speech/vyser se na blbou švadlenku.ogg"
+  },
+  {
+    "revision": "f3149b8994c14ffc476d1597a0d55ea9",
+    "url": "/statics/speech/všechno nejpojebanější.mp3"
+  },
+  {
+    "revision": "1940ffae78a85901b7cdb4a895869ac1",
+    "url": "/statics/speech/všechno nejpojebanější.ogg"
+  },
+  {
+    "revision": "5b05cd6c44b6617e26abca75ef5a0128",
+    "url": "/statics/speech/z kundy xichte.mp3"
+  },
+  {
+    "revision": "47e5bf891707bca34f768d41731f4bb3",
+    "url": "/statics/speech/z kundy xichte.ogg"
+  },
+  {
+    "revision": "976a7a5decaf783b03b0265e81a806ca",
+    "url": "/statics/speech/čau z kundy xichte.mp3"
+  },
+  {
+    "revision": "f07468f229d2dc73fa1347c12f90249c",
+    "url": "/statics/speech/čau z kundy xichte.ogg"
+  },
+  {
+    "revision": "c3d8dd5a582c775de2aefc8d3962c02b",
+    "url": "/statics/speech/šáhni ji na prdel.mp3"
+  },
+  {
+    "revision": "5400cd75ef317c0c48c3b287b2b3ddf6",
+    "url": "/statics/speech/šáhni ji na prdel.ogg"
+  }
+]);
